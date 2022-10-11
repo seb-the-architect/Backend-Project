618989bf9d19a4f554ede40991a18e4c3e3a2ae0
Seb <phaedyfinch@gmail.com>
2022-10-11 11:52:53 +0100

    Removed comments and c.logs

--- app.js
+++ app.js
@@ -3,15 +3,6 @@ const express = require("express");
 
 const app = express();
 
-/*
-GET /api/topics
--------------------
-Responds with: an array of topic objects, each of which should have the following properties:
-slug
-description
--------------------
-*/
-
 app.get("/api/topics", controller.getAllTopics)
 
 app.listen(5000, ()=>
--- model/sql-query-model.js
+++ model/sql-query-model.js
@@ -1,12 +1,7 @@
 const pool = require("../db/connection.js")
 
-//Database: nc-news
-//Table: topics
-
-//Returns all topics as a list of objects
 exports.queryAllTopics = async function()
 {
     const allTopics = await pool.query("SELECT * FROM topics");
-    console.log(allTopics.rows);
     return allTopics.rows;
 }
